26f5a88e680378ed776cd1500f95a5462ca9f6be
yachao_li <yachao.li@airbnb.com>
2020-05-12 17:33:02 +0800

    [Webpack]Add the optimize options and judge the environment and clean the dist directory when begin to build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyESPlugin = require('uglifyjs-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const isDev = process.env.NODE_ENV === 'development';
 
 // 负责将html文档虚拟到根目录下
 let htmlWebpackPlugin = new HtmlWebpackPlugin({
@@ -10,14 +13,14 @@ let htmlWebpackPlugin = new HtmlWebpackPlugin({
 })
 
 module.exports = {
-    mode: "development",
+    mode: isDev ? "development" : "production",
     entry: "./src/index.tsx",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "main.js"
     },
     // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+    devtool: isDev ? 'cheap-module-eval-source-map' : "source-map",
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".ts", ".tsx", ".js"]
@@ -72,6 +75,29 @@ module.exports = {
     },
     // 装载虚拟目录插件
     plugins: [
-        htmlWebpackPlugin
+        htmlWebpackPlugin,
+        new CleanWebpackPlugin(),
+        new UglifyESPlugin({
+            // 多嵌套了一层
+            uglifyOptions: {
+                compress: {
+                    // 在UglifyJs删除没有用到的代码时不输出警告
+                    warnings: false,
+                    // 删除所有的 `console` 语句，可以兼容ie浏览器
+                    drop_console: true,
+                    // 内嵌定义了但是只用到一次的变量
+                    collapse_vars: true,
+                    // 提取出出现多次但是没有定义成变量去引用的静态值
+                    reduce_vars: true,
+                },
+                output: {
+                    // 最紧凑的输出
+                    beautify: false,
+                    // 删除所有的注释
+                    comments: false,
+                }
+            }
+        })
+
     ],
 };
